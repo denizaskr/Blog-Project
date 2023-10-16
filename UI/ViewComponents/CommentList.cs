b0beecda0a385e9323cd8ab4494491f0aaa38eaf
Deniz <denizaskar05@gmail.com>
2023-10-16 16:37:55 +0300

    Add project files.

---
+++ UI/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using BlogProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BlogProject.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentValues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID = 1,
+					UserName="Deniz"
+				},
+				new UserComment {
+					ID=1,
+					UserName="Yakup"
+				},
+				new UserComment
+				{
+					ID=1,
+					UserName="Doga"
+				}
+			};
+			return View(commentValues);
+		}
+	}
+}
